4aa800fc7d46a4c1a2307c24ee068f0ee8507413
alanef <alan@fullworks.net>
2021-11-29 20:16:14 +0000

    first

--- PluginDonation.php
+++ PluginDonation.php
@@ -3,24 +3,28 @@
 namespace AlanEFPluginDonation;
 
 class PluginDonation {
-    protected $plugin_slug;
+	protected $plugin_slug;
 	protected $settings_hook;
-	public function __construct($plugin_slug, $settings_hook) {
+
+	public function __construct( $plugin_slug, $settings_hook ) {
 		$this->plugin_slug = $plugin_slug;
-		$this->hook = $settings_hook;
+		$this->hook        = $settings_hook;
 		$this->hooks();
 	}
 
 	private function hooks() {
-		add_action( 'admin_enqueue_scripts',  array($this, 'enqueue_styles' ));
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 	}
 
 	public function enqueue_styles( $hook ) {
 		if ( $hook === $this->settings_hook ) {
 			$this->add_inline_admin_style();
+
 			return;
 		}
 	}
+
 	private function add_inline_admin_style() {
 		$style = <<<EOT
 /* Style the tab */
@@ -63,4 +67,39 @@ EOT;
 		wp_add_inline_style( 'admin-bar', $style );
 	}
 
+
+	public function enqueue_scripts( $hook ) {
+		if ( $hook === $this->settings_hook ) {
+			$this->add_inline_admin_script();
+
+			return;
+		}
+	}
+
+	private function add_inline_admin_script() {
+		$script = <<<EOT
+function openCrypto(evt, cryptName) {
+    evt.preventDefault();
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+     tabcontent = document.getElementsByClassName("tabcontent");
+     for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+     }
+
+     // Get all elements with class="tablinks" and remove the class "active"
+     tablinks = document.getElementsByClassName("tablinks");
+     for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+     }
+
+     // Show the current tab, and add an "active" class to the button that opened the tab
+     document.getElementById(cryptName).style.display = "block";
+     evt.currentTarget.className += " active";
+}
+EOT;
+		wp_add_inline_style( 'admin-bar', $script );
+	}
 }
